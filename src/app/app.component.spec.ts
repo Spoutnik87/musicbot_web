5b0085372950720c9784f9319db15337eb656401
Vladyslav Dumas <vladkyry@gmail.com>
2019-07-21 11:38:34 +0200

    Removing ngrx store from tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,23 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { LetDirective } from './directives/let.directive';
-import { AuthService } from './services';
-import { metaReducers, reducers } from './store';
+import { ConfigService } from './services';
+import { AuthService } from './store/auth/auth.service';
+import { MessagesService } from './store/messages/messages.service';
+import { UsersService } from './store/users/users.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, FontAwesomeModule, StoreModule.forRoot(reducers, { metaReducers })],
-      declarations: [AppComponent, HeaderComponent, LoadingComponent, LetDirective],
-      providers: [AuthService],
+      imports: [RouterTestingModule, FontAwesomeModule, HttpClientTestingModule],
+      declarations: [AppComponent, HeaderComponent, LoadingComponent, LetDirective, FooterComponent],
+      providers: [AuthService, UsersService, ConfigService, MessagesService],
     }).compileComponents();
   }));
 
